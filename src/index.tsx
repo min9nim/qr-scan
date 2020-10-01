28576db155c26ab87059496b5c5d75ca60777c7f
min9nim <min9nim@gmail.com>
2020-10-01 22:57:35 +0900

    CRA 기본 serviceWorker.js 안 사용

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import logger from './utils/logger'
-import * as serviceWorker from './serviceWorker'
+// import * as serviceWorker from './serviceWorker'
 
 // @ts-ignore
 window.$logger = logger
@@ -18,4 +18,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register()
+// serviceWorker.register()
